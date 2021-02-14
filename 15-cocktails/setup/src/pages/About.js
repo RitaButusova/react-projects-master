aac0c34b6a10784c709f815b1c6e9707f34d620f
RitaButusova <47254480+RitaButusova@users.noreply.github.com>
2021-02-14 20:16:43 +0200

    Initial commit

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, necessitatibus ducimus? Laborum asperiores excepturi autem illo, vel sapiente eum iste accusantium aspernatur corporis neque, velit hic enim nulla consectetur harum?</p>
+    </section>
+  )
+}
+
+export default About
